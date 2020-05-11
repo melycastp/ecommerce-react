82579a94e5f945989801d3dc02942d1eb09ffa6a
mely_casta_eda <melycastp@gmail.com>
2020-05-10 23:51:58 -0500

    Updates

---
+++ precache-manifest.0f9847d4da33b853798bf69a41cacfbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d682adba1554f2079f8d52bebfe17e44",
+    "url": "/ecommerce-react/index.html"
+  },
+  {
+    "revision": "b37a6854db7b55fe8a30",
+    "url": "/ecommerce-react/static/css/main.0056fab8.chunk.css"
+  },
+  {
+    "revision": "734ed26a798110ac197d",
+    "url": "/ecommerce-react/static/js/2.d4f32309.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce-react/static/js/2.d4f32309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37a6854db7b55fe8a30",
+    "url": "/ecommerce-react/static/js/main.dbd90a56.chunk.js"
+  },
+  {
+    "revision": "fbc4c959ffc55b7177a3",
+    "url": "/ecommerce-react/static/js/runtime-main.229fec53.js"
+  }
+]);
